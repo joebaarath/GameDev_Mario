b627bcc8bdaaa4d84ab2d73f5cfba5b6f095eb6e
Joe Baarath <joebaarath@hotmail.com>
2023-10-27 17:02:17 +0800

    lab 4 check off

--- Assets/Scripts/PauseButtonController.cs
+++ Assets/Scripts/PauseButtonController.cs
@@ -10,10 +10,15 @@ public class PauseButtonController : MonoBehaviour, IInteractiveButton
     public Sprite pauseIcon;
     public Sprite playIcon;
     private Image image;
+    GameObject pausePanelHolder;
+    Transform pausePanel;
     // Start is called before the first frame update
     void Start()
     {
         image = GetComponent<Image>();
+        pausePanelHolder = GameObject.Find("PausePanelHolder");
+        pausePanel = pausePanelHolder.transform.GetChild(0); // For the first child
+        pausePanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,17 +27,50 @@ public class PauseButtonController : MonoBehaviour, IInteractiveButton
 
     }
 
+    public void PauseAllAudio()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Pause();
+        }
+    }
+
+    // Function to resume all music and sounds
+    public void ResumeAllAudio()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.UnPause();
+        }
+    }
+
+    void TogglePanel()
+    {
+        bool currentState = pausePanel.gameObject.activeSelf;
+        pausePanel.gameObject.SetActive(!currentState);
+    }
+
+
     public void ButtonClick()
     {
         Time.timeScale = isPaused ? 1.0f : 0.0f;
         isPaused = !isPaused;
+        TogglePanel();
+
         if (isPaused)
         {
             image.sprite = playIcon;
+            PauseAllAudio();
+
+
         }
         else
         {
             image.sprite = pauseIcon;
+            ResumeAllAudio();
+
         }
     }
 }
